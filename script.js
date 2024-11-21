65feb5f4a8b17bf9a761b9549a1efffb26f51ba7
kauanoliveirasales <00001118013748sp@al.educacao.sp.gov.br>
2024-11-21 12:17:12 -0300

    Primeira versão do código

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
+
+function toggleMenu() {
+    console.log("Função chamada!");
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+}
